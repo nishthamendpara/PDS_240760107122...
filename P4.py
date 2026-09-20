d59e13f241ab140e4089d418446b647ff5d9c924
nishthamendpara <nishthamendpara@gmail.com>
2026-09-20 13:41:13 +0530

    Create P4.py

---
+++ P4.py
@@ -0,0 +1,18 @@
+def function_name():
+    '''
+    '''
+    pass
+
+def HelloWorld():
+    print('Helloworld')
+HelloWorld()
+
+def HelloWorld():
+    print('Hello World, this is my first function.')
+print()
+
+def add(x, y):
+    return x + y
+print(add(5, 4))
+print(type(HelloWorld()))
+print(type(None))
